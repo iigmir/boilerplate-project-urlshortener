b8d9b110276280d0c401ad0bf499a8e52153fa1f
iigmir <roc120j@gmail.com>
2024-01-03 09:16:12 +0800

    Show URL by bodyParser

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+// Dotenv
+require("dotenv").config();
+// Packages
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+// app
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", urlencodedParser, function(req, res) {
+  res.json({ original_url: req.body.url });
 });
 
 app.listen(port, function() {
